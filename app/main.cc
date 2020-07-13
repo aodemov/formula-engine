edd995d9a058c87aa9c0bfb7bd27354996471568
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2020-07-13 17:00:29 +0500

    Add main app scratch pad

--- app/main.cc
+++ app/main.cc
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <string>
+
+#include "lexer.h"
 
 int main(int, char**) {
+  std::string input;
+  std::getline(std::cin, input);
+
+  formulaEngine::Scanner scanner(input);
+  formulaEngine::Lexer lexer(&scanner);
+
+  formulaEngine::Token token;
+  while ((token = lexer.ReadNext()).type != formulaEngine::Token::EOE) {
+    std::cout << "Found token " << token.value << " (" << token.type << ") at position (" << token.beginPosition << "..." << token.endPosition << ")." << std::endl;
+  }
 }
