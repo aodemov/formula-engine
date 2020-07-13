8bc4d7cba0d43ee55d773edb2dbfebc40f658275
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2020-07-13 16:59:29 +0500

    Add lexer tests

---
+++ tests/lexer-test.cc
@@ -0,0 +1,86 @@
+#include "gtest/gtest.h"
+#include "lexer.h"
+
+#include <vector>
+#include <tuple>
+#include <string>
+#include <iostream>
+
+TEST(LexerTest, BasicCase){
+  using namespace std;
+  using namespace formulaEngine;
+
+  // Token type, value, beginPosition, endPosition
+  vector<tuple<Token::TokenType, string, size_t, size_t>> v = {
+    make_tuple(Token::NUMBER, "12", 0, 2),
+    make_tuple(Token::SLASH, "/", 3, 4),
+    make_tuple(Token::NUMBER, "205", 4, 7),
+    make_tuple(Token::PERCENT, "%", 7, 8),
+    make_tuple(Token::NUMBER, "1", 9, 10),
+    make_tuple(Token::EOE, "", 10, 10),
+    make_tuple(Token::EOE, "", 10, 10),
+  };
+
+  string input = "12 /205% 1";
+  formulaEngine::Scanner scanner(input);
+  formulaEngine::Lexer lexer(&scanner);
+
+  for(auto it = v.begin(); it != v.end(); it++) {
+    auto tok = lexer.ReadNext();
+    ASSERT_EQ(tok.type, get<0>(*it));
+    ASSERT_EQ(tok.value, get<1>(*it));
+    ASSERT_EQ(tok.beginPosition, get<2>(*it));
+    ASSERT_EQ(tok.endPosition, get<3>(*it));
+  }
+}
+
+TEST(LexerTest, NewLine){
+  using namespace std;
+  using namespace formulaEngine;
+
+  // Token type, value, beginPosition, endPosition
+  vector<tuple<Token::TokenType, string, size_t, size_t>> v = {
+    make_tuple(Token::NUMBER, "453", 0, 3),
+    make_tuple(Token::NUMBER, "5", 7, 8),
+    make_tuple(Token::EOE, "", 8, 8),
+    make_tuple(Token::EOE, "", 8, 8),
+  };
+
+  string input = "453 \n\r 5";
+  formulaEngine::Scanner scanner(input);
+  formulaEngine::Lexer lexer(&scanner);
+
+  for(auto it = v.begin(); it != v.end(); it++) {
+    auto tok = lexer.ReadNext();
+    ASSERT_EQ(tok.type, get<0>(*it));
+    ASSERT_EQ(tok.value, get<1>(*it));
+    ASSERT_EQ(tok.beginPosition, get<2>(*it));
+    ASSERT_EQ(tok.endPosition, get<3>(*it));
+  }
+}
+
+
+TEST(LexerTest, Spaces){
+  using namespace std;
+  using namespace formulaEngine;
+
+  // Token type, value, beginPosition, endPosition
+  vector<tuple<Token::TokenType, string, size_t, size_t>> v = {
+    make_tuple(Token::NUMBER, "453", 0, 3),
+    make_tuple(Token::NUMBER, "5", 17, 18),
+    make_tuple(Token::EOE, "", 23, 23),
+    make_tuple(Token::EOE, "", 23, 23),
+  };
+
+  string input = "453              5     ";
+  formulaEngine::Scanner scanner(input);
+  formulaEngine::Lexer lexer(&scanner);
+
+  for(auto it = v.begin(); it != v.end(); it++) {
+    auto tok = lexer.ReadNext();
+    ASSERT_EQ(tok.type, get<0>(*it));
+    ASSERT_EQ(tok.value, get<1>(*it));
+    ASSERT_EQ(tok.beginPosition, get<2>(*it));
+    ASSERT_EQ(tok.endPosition, get<3>(*it));
+  }
+}
