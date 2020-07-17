9659f8db95a3d296c8ac2ab37ba624810cc16cba
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2020-07-17 18:02:35 +0500

    Add ast allocator class

---
+++ src/ast-allocator.cc
@@ -0,0 +1,68 @@
+#include "ast-allocator.h"
+
+namespace formulaEngine {
+
+void* AstAllocator::Allocate(size_t size) {
+  size = RoundAlign(size, kAlignment);
+  uintptr_t result = position;
+
+  if (size > top - position) {
+    result = Expand(size);
+  }
+
+  position += size;
+
+  return reinterpret_cast<void*>(result);
+}
+
+void AstAllocator::DeleteAll() {
+    AstAllocatorNode* current = head;
+
+    head = nullptr;
+
+    while (current) {
+      AstAllocatorNode* next = current->next();
+
+      DeallocateNode(current);
+      current = next;
+    }
+
+    position = 0;
+    top = 0;
+  }
+
+uintptr_t AstAllocator::Expand(size_t size) {
+  size_t oldSize = head ? head->total_size() : 0;
+  static const size_t kNodeOverhead = sizeof(AstAllocatorNode);
+  size_t newSize = size + (oldSize << 1) + kNodeOverhead;
+
+  size_t necessarySize = kNodeOverhead + size;
+
+  if (newSize < kMinimumExpandSize) {
+    newSize = kMinimumExpandSize;
+  } else if (newSize > kMaximumExpandSize) {
+    newSize = kMaximumExpandSize > necessarySize ? kMaximumExpandSize : necessarySize;
+  }
+
+  // TODO add nullptr check
+  AstAllocatorNode* newNode = AllocateNode(newSize);
+  newNode->setNext(head);
+  head = newNode;
+
+  top = newNode->end();
+
+  uintptr_t result = RoundAlign(newNode->begin(), kAlignment);
+  position = result;
+
+  return result;
+}
+
+AstAllocator::AstAllocatorNode* AstAllocator::AllocateNode (size_t size) {
+  void* memory = malloc(size);
+  return new (memory) AstAllocatorNode(size);
+}
+
+void AstAllocator::DeallocateNode (AstAllocatorNode* node) {
+  free(node);
+}
+}
---
+++ src/ast-allocator.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <stdlib.h>
+#include <new>
+
+namespace formulaEngine {
+// Efficiently allocates small chucks of memory for AstNodes
+// Frees all the memory at once
+class AstAllocator {
+protected:
+  class AstAllocatorNode {
+  private:
+    friend class AstAllocator;
+
+    size_t size_;
+    AstAllocatorNode* next_;
+
+    explicit AstAllocatorNode(size_t size)
+      : size_(size)
+      {}
+  public:
+    AstAllocatorNode* next() const {
+      return next_;
+    }
+
+    void setNext(AstAllocatorNode* next) {
+      next_ = next;
+    }
+
+    size_t begin() const {
+      return reinterpret_cast<size_t>(this) + sizeof(AstAllocatorNode);
+    }
+
+    size_t end() const{
+      return reinterpret_cast<size_t>(this) + size_;
+    }
+
+    size_t capacity() const {
+      return size_ - sizeof(AstAllocatorNode);
+    }
+
+    size_t total_size() const {
+      return size_;
+    }
+  };
+
+  AstAllocatorNode* head;
+  uintptr_t position;
+  uintptr_t top;
+  // Todo add total size counter
+
+  const size_t kAlignment = 8;
+  const size_t kMinimumExpandSize = 1024;
+  const size_t kMaximumExpandSize = 1024 * 8;
+
+  size_t RoundAlign(size_t size, size_t alignment);
+
+  void* Allocate(size_t size);
+  uintptr_t Expand(size_t size);
+  AstAllocatorNode* AllocateNode (size_t size);
+  void DeallocateNode (AstAllocatorNode* node);
+public:
+  AstAllocator() = default;
+  ~AstAllocator();
+
+  AstAllocator(const AstAllocator& other) = delete;
+  AstAllocator& operator=(const AstAllocator& other) = delete;
+
+  template <typename T, typename... Args>
+  T* New(Args&&... args) {
+    size_t size = RoundAlign(sizeof(T), kAlignment);
+    void* memory = Allocate(size);
+    return new (memory) T(std::forward<Args>(args)...);
+  }
+
+  void DeleteAll();
+};
+}
