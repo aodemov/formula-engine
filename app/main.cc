35f4b36e7ad822a46da39d7d6459cffda68e2e7a
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2020-07-17 23:04:19 +0500

    Modify scratchpad to work with engine

--- app/main.cc
+++ app/main.cc
@@ -2,16 +2,30 @@
 #include <string>
 #include <memory>
 
-#include "lexer.h"
+#include "engine.h"
 
 int main(int, char**) {
   std::string input;
-  std::getline(std::cin, input);
+  
 
-  formulaEngine::Lexer lexer(std::make_unique<formulaEngine::Scanner>(input));
+  // formulaEngine::Lexer lexer(std::make_unique<formulaEngine::Scanner>(input));
 
-  formulaEngine::Token token;
-  while ((token = lexer.ReadNext()).type != formulaEngine::Token::EOE) {
-    std::cout << "Found token " << token.value << " (" << token.type << ") at position (" << token.beginPosition << "..." << token.endPosition << ")." << std::endl;
+  // formulaEngine::Token token;
+  // while ((token = lexer.ReadNext()).type != formulaEngine::Token::EOE) {
+  //   std::cout << "Found token " << token.value << " (" << token.type << ") at position (" << token.beginPosition << "..." << token.endPosition << ")." << std::endl;
+  // }
+
+  while (true) {
+    std::getline(std::cin, input);
+
+    if (input == "q") {
+      break;
+    }
+
+    formulaEngine::Engine* engine = new formulaEngine::Engine();
+    int result = engine->Evaluate(input);
+    std::cout << "RES: " << result << std::endl;
   }
+
+  
 }
