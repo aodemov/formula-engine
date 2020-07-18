caeec5371f14f58ec443c6ac155de8e2acaa9168
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2020-07-18 17:33:19 +0500

    Fix lexer tests

--- tests/lexer-test.cc
+++ tests/lexer-test.cc
@@ -18,8 +18,8 @@ TEST(LexerTest, BasicCase){
     make_tuple(Token::NUMBER, "205", 4, 7),
     make_tuple(Token::PERCENT, "%", 7, 8),
     make_tuple(Token::NUMBER, "1", 9, 10),
-    make_tuple(Token::EOE, "", 10, 10),
-    make_tuple(Token::EOE, "", 10, 10),
+    make_tuple(Token::EOE, "EOE", 10, 10),
+    make_tuple(Token::EOE, "EOE", 10, 10),
   };
 
   string input = "12 /205% 1";
@@ -42,8 +42,8 @@ TEST(LexerTest, NewLine){
   vector<tuple<Token::TokenType, string, size_t, size_t>> v = {
     make_tuple(Token::NUMBER, "453", 0, 3),
     make_tuple(Token::NUMBER, "5", 7, 8),
-    make_tuple(Token::EOE, "", 8, 8),
-    make_tuple(Token::EOE, "", 8, 8),
+    make_tuple(Token::EOE, "EOE", 8, 8),
+    make_tuple(Token::EOE, "EOE", 8, 8),
   };
 
   string input = "453 \n\r 5";
@@ -67,8 +67,8 @@ TEST(LexerTest, Spaces){
   vector<tuple<Token::TokenType, string, size_t, size_t>> v = {
     make_tuple(Token::NUMBER, "453", 0, 3),
     make_tuple(Token::NUMBER, "5", 17, 18),
-    make_tuple(Token::EOE, "", 23, 23),
-    make_tuple(Token::EOE, "", 23, 23),
+    make_tuple(Token::EOE, "EOE", 23, 23),
+    make_tuple(Token::EOE, "EOE", 23, 23),
   };
 
   string input = "453              5     ";
